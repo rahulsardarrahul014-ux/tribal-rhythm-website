1880db023d039c9f62d4e7119be41eb02711634f
rahulsardarrahul014-ux <rahulsardarrahul014@gmail.com>
2026-05-07 19:10:32 +0530

    Fixed firebase private key

--- backend/server.js
+++ backend/server.js
@@ -22,12 +22,14 @@ app.use(cors({
 }));
 
 // ================= FIREBASE INIT =================
-const serviceAccount = JSON.parse(
-    process.env.FIREBASE_KEY.replace(/\\n/g, '\n')
-);
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+};
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount)
 });
 
 const db = admin.firestore();
