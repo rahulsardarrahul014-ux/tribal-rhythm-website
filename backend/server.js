5f0e1354f97db9557098c5f8a1e62cfd46b3f78c
rahulsardarrahul014-ux <rahulsardarrahul014@gmail.com>
2026-07-09 21:04:16 +0530

    Fix CORS

--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,23 @@ app.use(express.json({
     }
 }));
 
+const allowedOrigins = [
+    "https://rahulsardarrahul014-ux.github.io"
+];
+
 app.use(cors({
-    origin: process.env.FRONTEND_URL
+    origin: function (origin, callback) {
+
+        if (!origin) return callback(null, true);
+
+        if (allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+
+    },
+    credentials: true
 }));
 
 // ================= FIREBASE INIT =================
@@ -164,20 +179,20 @@ app.post("/verify-otp", async (req, res) => {
 app.post("/create-order", async (req, res) => {
     try {
         const { amount, email } = req.body;
-        console .log("Amount received:" ,amount);
-        console.log("Amount type:",typeof amount);
-        console.log("Amount sent to Razorpay:",amount*100);
+        console.log("Amount received:", amount);
+        console.log("Amount type:", typeof amount);
+        console.log("Amount sent to Razorpay:", amount * 100);
 
         if (!amount || !email || !isValidEmail(email)) {
             return res.status(400).json({ message: "Invalid request" });
         }
 
-        const finalAmount =Number (amount)*100;
+        const finalAmount = Number(amount) * 100;
         console.log(finalAmount);
 
         const order = await razorpay.orders.create({
             // amount: amount * 100, // rupees → paise
-            amount:finalAmount,
+            amount: finalAmount,
             currency: "INR",
             receipt: "receipt_" + Date.now(),
             notes: {
