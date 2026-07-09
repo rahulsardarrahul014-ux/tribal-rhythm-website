b15d591ddbc594f78c98b76c6e66d3d2fc83c196
rahulsardarrahul014-ux <rahulsardarrahul014@gmail.com>
2026-07-09 21:11:45 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,8 @@ app.use(express.json({
 }));
 
 const allowedOrigins = [
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
     "https://rahulsardarrahul014-ux.github.io"
 ];
 
@@ -27,9 +29,9 @@ app.use(cors({
 
         if (allowedOrigins.includes(origin)) {
             callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
         }
+
+        callback(new Error("Not allowed by CORS"));
 
     },
     credentials: true
