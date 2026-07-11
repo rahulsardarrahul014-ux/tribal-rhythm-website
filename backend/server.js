af84d37cc55a7d4a45bb6cd9a92f672891489086
rahulsardarrahul014-ux <rahulsardarrahul014@gmail.com>
2026-07-11 12:56:57 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -44,6 +44,9 @@ const serviceAccount = {
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     privateKey: (process.env.FIREBASE_PRIVATE_KEY || "").replace(/\\n/g, "\n")
 };
+console.log("PROJECT_ID:", process.env.FIREBASE_PROJECT_ID);
+console.log("CLIENT_EMAIL:", process.env.FIREBASE_CLIENT_EMAIL);
+console.log("PRIVATE_KEY:", process.env.FIREBASE_PRIVATE_KEY ? "Loaded" : "Missing");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
