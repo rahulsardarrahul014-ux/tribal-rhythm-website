d8f7ade7627e391d4c1a596d087c8410b8147d64
rahulsardarrahul014-ux <rahulsardarrahul014@gmail.com>
2026-07-11 17:54:00 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -87,8 +87,10 @@ console.log("SECRET :", process.env.RAZORPAY_KEY_SECRET ? "Loaded" : "Missing");
 // ================= EMAIL =================
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
-
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    requireTLS: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
